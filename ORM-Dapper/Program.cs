d0d9961c194f01fb3ea67a0571397bf1019545cf
katherine Buendia <kbbuendia24@gmail.com>
2023-06-08 09:50:37 -0400

    ORM Exercise one

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace ORM_Dapper
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
